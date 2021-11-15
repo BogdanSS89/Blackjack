e446c1ddaa124fa575cd404da1e9fa8ebc06590a
BogdanSS <restoff89@gmail.com>
2021-11-15 14:55:05 +0200

    Добавьте файлы проекта.

---
+++ Hand.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Card.h"
+
+using namespace std;
+
+class Hand
+{
+protected:
+	vector<Card*> m_Cards;
+public:
+	Hand();
+
+	void Add(Card* pCard);
+	void Clear();
+	int GetTotal() const;
+
+	virtual ~Hand();
+};
---
+++ House.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "GenericPlayer.h"
+
+class House :
+    public GenericPlayer
+{
+public:
+    House(const string& name = "House");
+    virtual ~House();
+
+    virtual bool IsHitting() const;
+    void FlipFirstCard();
+};
